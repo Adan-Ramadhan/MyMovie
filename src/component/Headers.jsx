26784491494f9d81d9fbdd80d094c6347363b8a8
Adan <adanramadhan2001@gmail.com>
2024-07-09 19:12:56 +0700

    create navbar

--- src/component/Headers.jsx
+++ src/component/Headers.jsx
@@ -1,12 +1,20 @@
 import Search from "./Search";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { RxCross2 } from "react-icons/rx";
 
+import { useState } from "react";
 const Headers = () => {
+  const [navbar, setNavbar] = useState(false);
  
-
+  const handleNavbar = () => {
+    setNavbar(!navbar);
+  }
+  
   return (
-    <div className="w-full min-h-auto absolute z-10">
-      {/* for mobile navbar */}
-      <div className="w-full min-h-auto bg-red-400">
+    <div className={`w-full min-h-auto absolute z-10 ${navbar ? "bg-slate-700" : "bg-transparent"}`}>
+      <div className="w-full flex justify-end p-2 z-10 text-xl"><button onClick={handleNavbar}> {navbar ?  <RxCross2 /> : <RxHamburgerMenu/>}</button></div>
+
+      <div className={`w-full min-h-auto  ${navbar ? "translate-y-0 transition-all duration-300" : "-translate-y-80 transition-all duration-300"}  `}>
         <Search />
 
       </div>
--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import {  useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const Search = ({ onSearch }) => {
+
+const Search = () => {
   const [query, setQuery] = useState("");
   const navigate = useNavigate();
-
 
   const handleInputChange = (e) => {
     setQuery(e.target.value);
@@ -13,25 +14,34 @@ const Search = ({ onSearch }) => {
   const handleSearch = (e) => {
     // onSearch(query);
     e.preventDefault();
-    if(query.trim()){
+    if (query.trim()) {
       navigate(`/search/${query}`);
-    }else{
-      alert("Query tidak boleh kosong")
+    } else {
+      alert("Query tidak boleh kosong");
     }
-    
-    
   };
-  
-  
-  
+
+
+
   return (
     <div>
-      <form onSubmit={handleSearch} className="flex items-center">
-        <input type="text" value={query} placeholder="Search..." onChange={handleInputChange} className="px-3 py-2 border text-slate-700 rounded-lg" />
-        <button type="submit" className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-lg">
-        Search
-        </button>
-      </form>
+      {/* for navbar mobile */}
+      <div className="w-full p-3 ">
+        <form onSubmit={handleSearch} className="flex items-center mb-4">
+          <input type="text" value={query} placeholder="Search..." onChange={handleInputChange} className="w-full rounded-bl-lg rounded-tl-lg px-3 py-2 border text-slate-700 " />
+          <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded-tr-lg rounded-br-lg">
+            Search
+          </button>
+        </form>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
